15bfa1853b39578cc08ed32c23ddd31efdfa734a
DMendez455 <106936869+DMendez455@users.noreply.github.com>
2024-05-04 12:33:42 -0600

    Add files via upload

---
+++ progra1-p2-2024-main/Parcial-2/Parcial-2/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Parcial-2")]
+[assembly: AssemblyDescription("Descripción del ensamblado.")]
+[assembly: AssemblyConfiguration("Configuración del ensamblado.")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Parcial-2")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("Marcas comerciales.")]
+[assembly: AssemblyCulture("Cultura del ensamblado (por ejemplo, 'es', 'en', etc.).")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("73c3dec9-6dc6-44ab-9002-b55ba4f83a0a")]
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Si deseas permitir el acceso a tipos internos desde otro ensamblado, descomenta y modifica la línea siguiente.
+// [assembly: InternalsVisibleTo("NombreDelOtroEnsamblado")]
+
